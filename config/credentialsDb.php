ff66e7ac3f4d00df1b7f86afe19fb579993e2326
florrvargas <florencia.vargas@goup.cloud>
2023-07-12 17:08:36 -0300

    con credentialsDB

---
+++ config/credentialsDb.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST', 'containers-us-west-129.railway.app');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'HI8OIRqZdpanOlfXd5cu');
+    define('DB_NAME', 'railway');
+?>
